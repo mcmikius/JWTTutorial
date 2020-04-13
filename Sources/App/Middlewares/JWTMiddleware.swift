e5097d0c8fb1dd89278013ea92d8bc5769a5e2a4
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-04-13 22:27:42 +0300

    fix middleware

--- Sources/App/Middlewares/JWTMiddleware.swift
+++ Sources/App/Middlewares/JWTMiddleware.swift
@@ -12,27 +12,16 @@ public final class JWTMiddleware: Middleware {
     
     public func respond(to req: Request, chainingTo next: Responder) throws -> EventLoopFuture<Response> {
         
-        guard let bearer = req.http.headers.bearerAuthorization else {
-            throw Abort(.unauthorized)
+        if let token = req.http.headers[.authorization].first {
+            do {
+                try JWT<AccessTokenPayload>(from: token, verifiedUsing: JWTConfig.signer)
+                return try next.respond(to: req)
+            } catch let error as JWTError {
+                throw Abort(.unauthorized, reason: error.reason)
+            }
+        } else {
+            throw Abort(.unauthorized, reason: "No Access Token")
         }
-        
-        //        let jwt = try JWT<User>(from: bearer.token, verifiedUsing: .hs256(key: "secret"))
-        _ = try JWT<AccessTokenPayload>(from: bearer.token, verifiedUsing: JWTConfig.signer)
-        return try next.respond(to: req)
-        
-        //        return "Hello, \(jwt.payload.name)!"
-        
-        //        if let token = req.http.headers[.authorization].first {
-        //            do {
-        //
-        //                try JWT<AccessTokenPayload>(from: token, verifiedUsing: JWTConfig.signer)
-        //                return try next.respond(to: req)
-        //            } catch let error as JWTError {
-        //                throw Abort(.unauthorized, reason: error.reason)
-        //            }
-        //        } else {
-        //            throw Abort(.unauthorized, reason: "No Access Token")
-        //        }
     }
 }
 
